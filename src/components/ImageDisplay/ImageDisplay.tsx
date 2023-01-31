500616c6b19da01d8f6e1a3901c635d86a851fb6
Marc Girbau <marc.girbau94@gmail.com>
2023-01-31 21:52:07 +0100

    Add ImageDisplay component

---
+++ src/components/ImageDisplay/ImageDisplay.tsx
@@ -0,0 +1,9 @@
+const ImageDisplay = (): JSX.Element => {
+  return (
+    <div className="container-gif offset-1 col-6">
+      <img src="" alt="" className="gif" />
+    </div>
+  );
+};
+
+export default ImageDisplay;
